e22fb2fb07855e9d08abc0d00f2cff5df468fd2d
Zoldur <36329578+zoldur@users.noreply.github.com>
2018-08-19 20:36:40 +0100

    Update fossil_install.sh

--- fossil_install.sh
+++ fossil_install.sh
@@ -116,7 +116,7 @@ function update_config() {
   sed -i 's/daemon=1/daemon=0/' $CONFIGFOLDER/$CONFIG_FILE
   cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
 logintimestamps=1
-maxconnections=256
+maxconnections=16
 #bind=$NODEIP
 masternode=1
 externalip=$NODEIP:$COIN_PORT
